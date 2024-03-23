020e50533da6c970c7bc92802e2d292af1bc6213
Matheo Godenzi <matheo.godenzi@epfl.ch>
2024-03-23 17:15:04 +0100

    auto-analysis

--- auto_analysis.py
+++ auto_analysis.py
@@ -5,3 +5,23 @@ Created on Sat Mar 23 14:21:41 2024
 @author: matheo
 """
 
+"""libraries import"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+import scipy as sp
+import sklearn as skl
+import pandas as pd
+import os
+import seaborn as sb
+from datetime import datetime
+import matplotlib.dates as mdates
+
+"""functions imports"""
+
+import functions as f
+import controls as c
+
+
+"""data acquisition"""
+
