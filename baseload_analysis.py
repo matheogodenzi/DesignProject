716b521ac1c4119f51d08d552ce893bd4b692552
Matheo Godenzi <matheo.godenzi@epfl.ch>
2024-04-12 19:22:15 +0200

    baseload done

--- baseload_analysis.py
+++ baseload_analysis.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar  8 14:53:58 2024
+Created on Fri Apr 12 14:30:43 2024
 
 @author: matheo
 """
@@ -97,6 +97,7 @@ Building_dict_2023 = {f.get_variable_name(Commune_paths[i], globals()): building
 pv_2022_dict = {pv_commune[i]: pv_2022[i] for i in range(len(pv_commune))}
 
 print(pv_2022_dict)
+
 #%% get all typologies sorted for all provided year 
 
 #School_loads =[]
@@ -135,7 +136,7 @@ Tendency = f.period_tendencies(Loads, Period)
 
 
 #extracting 1 single load to compare with the benchmark and giving it the same smoothness 
-single_load = Typo_all_loads[Typology].iloc[:, 4].to_frame()
+single_load = Typo_all_loads[Typology].iloc[:, 0].to_frame()
 #print(single_load)
 smoothed_load = f.period_tendencies(single_load, Period)
 
@@ -143,130 +144,43 @@ smoothed_load = f.period_tendencies(single_load, Period)
 # plotting 
 updated_tendency = f.plot_mean_load(smoothed_load, Tendency, Period, Typology)
 
+
 #%% creating a benchmark over available years
 
-# Obtain a typical year
+# Obtain a typical year averaged
 typical_year = f.typical_period(Loads,  "year")
 
-#%% Comparing typologies to distinguish benchmarks
-typical_day = f.typical_period(typical_year, Period)
 
-#color list 
-color = ["darkblue", "royalblue", "green", "yellow", "orange", "red", "purple", ]
+Loads_2022 = Typo_loads_2022[Typology]
+Loads_2023 = Typo_loads_2023[Typology]
+#%%
 
-#initiating figure
-plt.figure()
-for i, typo in enumerate(Typo_list) : 
+
+# Convert index to datetime type if it's not already
+Loads_2022.index = pd.to_datetime(Loads_2022.index)
+
+baseload_df = pd.DataFrame(columns=Loads_2022.columns)
+
+i = 0
+for (year, month, day), group in Loads_2022.groupby([Loads_2022.index.year, Loads_2022.index.month, Loads_2022.index.day]):
+    #data_by_day.append(group)
     
-    loads = Typo_all_loads[typo]
-    # Obtain a typical year
-    t_year = f.typical_period(loads,  "year")
-    #obtain typical day 
-    t_day = f.typical_period(t_year, "day")
+    # Calculate the average of the smallest values of each column within each group
+    average_of_smallest_values = group.apply(lambda x: x.nsmallest(6).mean())
     
-    word_to_find = " Renens"
     
-    if typo == "Ecole":
-        # Get the column names containing the word
-        matching_columns = [col for col in t_day.columns if word_to_find.lower() in col.lower()]
-        
-        # Set colors for matching and non-matching columns
-        colors = ['pink' if col in matching_columns else 'darkblue' for col in t_day.columns]
-        for i, col in enumerate(t_day.columns):
-            t_day[col].plot(color=colors[i], label=typo)
-
-    else:
-        plt.semilogy(t_day, color=color[i], label=typo)
+    print(day, month)
+    # Display the result
+    print(average_of_smallest_values)
+    print()
+    i+=1
     
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', title="Typologies")
-plt.tight_layout(rect=[0, 0, 1, 2.3])
-plt.xlabel("Heures")
-plt.ylabel("Mean daily consumption [$kWh_{el}/m^2$]")
-plt.title("Mean day consumption - typology distinction")
-plt.grid()
-plt.show()
-#%% plotting one specific load over given benchmark 
 
-Load1 = typical_day.iloc[:, 7].to_frame()
-f.plot_mean_load(Load1, typical_day, Period, Typology)
-
-
-
-#%% test of plotting benchark averages alone 
-
-# Annual weekly smoothing of the loag curve
-tendency = f.period_tendencies(typical_year, Period)
-
-f.plot_mean_load(None, tendency, period=Period, Typology=Typology)
-
-
-#%% All benchmark displayed  for a day
-
-
-#data = Typo_loads["Apems"]
-Period = "day"
-
-tendency_day = f.period_tendencies(Loads, Period)
-data_day = f.typical_period(Loads, Period)
-
-# Typical day for all infrastructures 
-f.plot_typical_day(data_day, Typology)
-
-
-#daily smoothing along the year for all insfrastrctures 
-f.plot_tendency(tendency_day.head(365), title="Load curve weekly average for "+Typology+"s", period=Period, show_legend=True)
-
-
-#%% All Benchmark plotted for a week 
-
-#data = Typo_loads["Apems"]
-Period = "week"
-
-# Annual weekly smoothing of the loag curve
-tendency_week = f.period_tendencies(Loads, Period)
-data_week = f.typical_period(Loads, Period)
-
-
-#typical week for all infrastructures 
-f.plot_typical_week(data_week, Typology)
-
-#weekly smoothing along the year for all insfrastrctures 
-f.plot_tendency(tendency_week, title="Load curve weekly average for "+Typology+"s", period=Period, show_legend=True)
-
-
-#%% extraction of a given week 
-
-
-Typology = "Ecole"
-Period = "week"
-
-# smoothing calculations
-Loads = Typo_all_loads[Typology]
-
-typical_week = f.typical_period(Loads, Period)
-
-
-#extracting 1 single load to compare with the benchmark and giving it the same smoothness 
-single_load = Typo_all_loads[Typology].iloc[:, 6].to_frame()
-#print(single_load)
-interest_period = aa.extract_period(single_load, pd.to_datetime("14.01.2023 00:15:00"), pd.to_datetime("21.01.2023 00:00:00"))
-
-# plotting 
-updated_tendency = f.plot_mean_load(interest_period, typical_week, Period, Typology)
-
-#%% Extracting all instances of the same exact time
-
-#time_of_interest = aa.extract_time(Load, pd.Timestamp('00:00:00'))
-
-
-#%% align years
-
-
-
-for column_name in Loads.columns : 
+"""
+for column_name in Loads_2022.columns : 
     
     # Calculate the 10th percentile value for the chosen column
-    percentile_10 = Loads[column_name].quantile(0.2)
+    percentile_10 = Loads[column_name].quantile(0.1)
     
     # Extract the values from the chosen column that are less than or equal to the 10th percentile
     lowest_10_percentile = Loads[Loads[column_name] <= percentile_10][column_name]
@@ -274,7 +188,48 @@ for column_name in Loads.columns :
     plt.plot(lowest_10_percentile)
     plt.show()
     exit()
+"""
+#%%
+import pandas as pd
+
+# Create an empty DataFrame with columns for 'Day' and 'Month'
+results_df = pd.DataFrame(columns=Loads_2022.columns)
+
+for (year, month, day), group in Loads_2022.groupby([Loads_2022.index.year, Loads_2022.index.month, Loads_2022.index.day]):
+    # Calculate the average of the smallest values of each column within each group
+    average_of_smallest_values = group.apply(lambda x: x.nsmallest(96).mean())
+    print(group.shape)
+    # Create a DataFrame for the current iteration
+    current_df = pd.DataFrame(average_of_smallest_values).T  # Transpose to make it a row
+    #current_df['Day'] = day
+    #current_df['Month'] = month
+    
+    # Set datetime index for the current iteration
+    current_df.index = pd.to_datetime([f'{year}-{month}-{day}'])
+    
+    # Append the DataFrame to the results DataFrame
+    results_df = pd.concat([results_df, current_df])
+
+# Display the filled DataFrame
+print(results_df)
+
+#%%
+plt.figure()
+plt.plot(results_df)
+plt.legend([i for i in range(results_df.shape[0])])
+plt.show
+
+plt.figure()
+plt.plot(results_df.iloc[:, 7])
+plt.show()
+
+#%% test 
+
+Period = "day"
+
+tendency_day = f.period_tendencies(Loads_2022, Period)
 
 
-
+plt.plot(results_df.iloc[:,6].values[:-1] - tendency_day.iloc[:,6].values)
+plt.show()
 
